14965e13e6f1e7fddc20af9313a7bc24fab0c5b2
Nour-elwshahy <139360490+Nour-elwshahy@users.noreply.github.com>
2024-09-22 21:50:12 +0300

    Add files via upload

---
+++ javaa/javaa/javaa/javaa/appp.js
@@ -0,0 +1,132 @@
+// let parent = document.getElementById("parent");
+// parent.style.display = "flex";
+// parent.style.justifyContent = "space-between";
+// parent.style.padding = "20px";
+
+// let box1 = document.getElementById("box1");
+// let card = `
+//   <div id="box1">
+//     <h1 id="heading1">Breaking News</h1>
+//     <img id="image1" src="images/coding.jpg" alt="photo" decoding="async" />
+//     <a id="link1" href="https://www.facebook.com/">
+//       link
+//     </a>
+//     <p id="paragraph1">Hello world</p>
+//   </div>`;
+// box1 = card.repeat(4);
+// box1.style.flex = "1";
+// box1.style.margin = "10px";
+// box1.style.padding = "20px";
+// box1.style.border = "1px solid black";
+// box1.style.backgroundColor = "#F4C6C6";
+// box1.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+// box1.style.textAlign = "center";
+
+// // let box2 = document.getElementById("box2");
+// // box2.style.flex = "1";
+// // box2.style.margin = "10px";
+// // box2.style.padding = "20px";
+// // box2.style.border = "1px solid black";
+// // box2.style.backgroundColor = "#C4F4C6";
+// // box2.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+// // box2.style.textAlign = "center";
+
+// // let box3 = document.getElementById("box3");
+// // box3.style.flex = "1";
+// // box3.style.margin = "10px";
+// // box3.style.padding = "20px";
+// // box3.style.border = "1px solid black";
+// // box3.style.backgroundColor = "#C6C6F4";
+// // box3.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+// // box3.style.textAlign = "center";
+
+// // let image1 = document.getElementById("image1");
+// // image1.style.width = "100%";
+// // image1.style.height = "auto";
+// // image1.style.marginBottom = "10px";
+
+// // let image2 = document.getElementById("image2");
+// // image2.style.width = "100%";
+// // image2.style.height = "auto";
+// // image2.style.marginBottom = "10px";
+
+// // let image3 = document.getElementById("image3");
+// // image3.style.width = "100%";
+// // image3.style.height = "auto";
+// // image3.style.marginBottom = "10px";
+
+// // //   [image1, image2, image3].forEach((image) => {
+// // //     image.style.width = "100%";
+// // //     image.style.height = "auto";
+// // //     image.style.marginBottom = "10px";
+// // //   });
+
+// // let heading1 = document.getElementById("heading1");
+// // let heading2 = document.getElementById("heading2");
+// // let heading3 = document.getElementById("heading3");
+
+// // [heading1, heading2, heading3].forEach((heading) => {
+// //   heading.style.fontSize = "24px";
+// //   heading.style.fontWeight = "bold";
+// //   heading.style.marginBottom = "10px";
+// // });
+
+// // let link1 = document.getElementById("link1");
+// // let link2 = document.getElementById("link2");
+// // let link3 = document.getElementById("link3");
+
+// // [link1, link2, link3].forEach((link) => {
+// //   link.style.display = "block";
+// //   link.style.color = "purple";
+// //   link.style.marginBottom = "10px";
+// //   link.style.textDecoration = "underline";
+// // });
+
+// // let paragraph1 = document.getElementById("paragraph1");
+// // let paragraph2 = document.getElementById("paragraph2");
+// // let paragraph3 = document.getElementById("paragraph3");
+
+// // [paragraph1, paragraph2, paragraph3].forEach((paragraph) => {
+// //   paragraph.style.fontSize = "16px";
+// //   paragraph.style.color = "black";
+// // });
+// // // let imo = (
+// // //   <div>
+// // //     <img
+// // //       id="image2"
+// // //       src="images\a125bb096ce5b8c479af41039830140a.jpg"
+// // //       alt="photo"
+// // //       decoding="async"
+// // //     />
+// // //   </div>
+// // // );
+// // // document.write(imo.repeat, 10);
+// console.log(
+//   "%cstop!",
+//   "color: white; background-color: blue; font-size: 20px; padding: 5px; border-radius: 3px"
+// );
+
+// let name = "nour elwshahy",
+//   age = 20,
+//   country = "egypt",
+//   job = "web developer",
+//   hobby = "painting";
+
+// let div = `
+//   <div>
+//     <h2>${job}</h2>
+//     <p>Welcome... my name is ${name}... I am ${age} years old and I live in ${country}.</p>
+//     <h4>My hobby is ${hobby}</h4>
+//   </div>
+// `;
+
+// document.write(div.repeat(5));
+
+const name = "NOUR ELWSHAHY";
+const hobby = "DRAWING";
+const quote = "If you cannot do great things, do small things in a great way.";
+
+const message = `Hello, my name is ${name}.\n\tMy favorite hobby is ${hobby}.\n\tA favorite quote of mine is: "${quote}"`;
+
+console.log(message);
+document.write(message);
